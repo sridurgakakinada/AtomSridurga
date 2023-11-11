21975d74fb0bc2d4c5b2a7945404a4e78bec4fe9
miabu-pashh <mdmaibubpasha@gmail.com>
2023-11-10 22:20:03 -0600

    sprint3 commit from maibu

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -1,48 +1,53 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import React from "react";
 import "./css/GetStarted.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import UserDashboard from "./UserDashboard";
 
 function GetStarted() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  // const history = useHistory();
+  const navigate = useNavigate();
 
+  const validateForm = () => {
+    let valid = true;
+
+    if (!email) {
+      setEmailError("Email is required");
+      valid = false;
+    } else if (!isValidEmail(email)) {
+      setEmailError("Invalid email format");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (!password) {
+      setPasswordError("Password is required");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    return valid;
+  };
+  const isValidEmail = (email) => {
+    // Regular expression for a simple email format validation
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
   const handleLogin = () => {
-    // Create an object with the user's credentials
-    const userCredentials = {
-      email,
-      password,
-    };
-
-    // Send a POST request to the Java backend using fetch
-    fetch("http://localhost:8090/api/v1/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userCredentials),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json(); // Parse the response as JSON
-        } else {
-          throw new Error("Login failed");
-        }
-      })
-      .then((data) => {
-        // Handle the successful response, e.g., store user data or token
-        console.log("Login was successful", data);
-
-        // You can redirect to another component after successful login if needed.
-        // For example, using React Router:
-        // history.push("/dashboard"); // Make sure to import useHistory from 'react-router-dom' and configure your routes
-      })
-      .catch((error) => {
-        // Handle errors, e.g., show an error message
-        console.error("Login failed:", error);
-      });
-
-    setEmail("");
-    setPassword("");
+    if (validateForm()) {
+      // The form is valid, you can proceed with login logic here.
+      console.log("Login button clicked");
+      // history.push("/UserDashboard");
+      navigate("/UserDashboard");
+      // Send the POST request to your backend.
+    }
   };
 
   return (
@@ -50,69 +55,79 @@ function GetStarted() {
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
-            {/* Your image */}
+            <img
+              src="https://www.hayeslocums.com/wp-content/uploads/2023/03/Hayes-Locums-NationalDoctorsDay-1024x498.jpg.webp"
+              className="img-fluid"
+              alt="Sample image"
+              style={{ height: "100%", width: "100%" }}
+            />
           </div>
-          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1 " style={{ marginTop: "80px" }}>
-            <div className="form-outline mb-4">
-              <input
-                type="email"
-                id="form3Example3"
-                className="form-control form-control-lg"
-                placeholder="Enter a valid email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <label className="form-label" htmlFor="form3Example3">
-                Email address
-              </label>
-            </div>
-
-            <div className="form-outline mb-3">
-              <input
-                type="password"
-                id="form3Example4"
-                className="form-control form-control-lg"
-                placeholder="Enter password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <label className="form-label" htmlFor="form3Example4">
-                Password
-              </label>
-            </div>
-
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="form-check mb-0">
+          <div
+            className="col-md-8 col-lg-6 col-xl-4 offset-xl-1"
+            style={{ marginTop: "80px" }}>
+            <form>
+              <div className="form-outline mb-4">
                 <input
-                  className="form-check-input me-2"
-                  type="checkbox"
-                  value=""
-                  id="form2Example3"
+                  type="email"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-                <label className="form-check-label" htmlFor="form2Example3">
-                  Remember me
+                <label className="form-label" htmlFor="form3Example3">
+                  Email address
                 </label>
+                <div className="text-danger">{emailError}</div>
               </div>
-              <a href="#!" className="text-body">
-                Forgot password?
-              </a>
-            </div>
 
-            <div className="text-center text-lg-start mt-4 pt-2">
-              <button
-                type="button"
-                style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                onClick={handleLogin}
-              >
-                Login
-              </button>
-              <p className="small fw-bold mt-2 pt-1 mb-0">
-                Don't have an account?{" "}
-                <a href="#!" className="link-danger">
-                  <Link to="/Register">Register</Link>
+              <div className="form-outline mb-3">
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="form-label" htmlFor="form3Example4">
+                  Password
+                </label>
+                <div className="text-danger">{passwordError}</div>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="form-check mb-0">
+                  <input
+                    className="form-check-input me-2"
+                    type="checkbox"
+                    value=""
+                    id="form2Example3"
+                  />
+                  <label className="form-check-label" htmlFor="form2Example3">
+                    Remember me
+                  </label>
+                </div>
+                <a href="#!" className="text-body">
+                  Forgot password?
                 </a>
-              </p>
-            </div>
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  type="button"
+                  style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                  onClick={handleLogin}>
+                  Login
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Don't have an account?{" "}
+                  <a href="#!" className="link-danger">
+                    <Link to="/Register">Register</Link>
+                  </a>
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
---
+++ src/components/OrganDetail.js
@@ -0,0 +1,84 @@
+// OrganDetail.jsx
+
+import React from "react";
+import "./css/OrganDetail.css";
+import { useState } from "react";
+
+function OrganDetail(props) {
+  const relatedDiseases = [
+    "Arrhythmia: This condition refers to an irregular heartbeat. Arrhythmias can be caused by a number of factors, including CAD, heart failure, and certain medications.",
+    "Heart valve disease: This condition occurs when one or more of the heart valves does not function properly. Heart valve disease can be caused by a number of factors, including infection, birth defects, and aging.",
+    "Heart failure: This condition occurs when the heart is unable to pump enough blood to meet the body's needs. Heart failure can be caused by a number of factors, including CAD, high blood pressure, and diabetes.",
+  ];
+  const doctorDetails = {
+    nameDoctor: "Dr. John Doe",
+    specialization: "Cardiologist",
+    hospital: "City Hospital",
+  };
+  const [userQuery, setUserQuery] = useState(" ");
+  // doctorDecorator();
+
+  ///Decorator Pattern
+
+  const doctorDecorator = (doctorDetails, relatedDiseases) => {
+    return {
+      ...doctorDetails,
+      relatedDiseases,
+      printDetails: function () {
+        console.log(`Doctor: ${this.nameDoctor}`);
+        console.log(`Specialization: ${this.specialization}`);
+        console.log(`Hospital: ${this.hospital}`);
+        console.log("Related Diseases:");
+        this.relatedDiseases.forEach((disease) => console.log(`- ${disease}`));
+      },
+    };
+  };
+
+  // Usage
+  const decoratedDoctor = doctorDecorator(doctorDetails, relatedDiseases);
+  decoratedDoctor.printDetails();
+
+  return (
+    <div className="organ-detail">
+      {/* Left Section */}
+      <div className="left-section">
+        <div className="organ-info">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/954/954406.png"
+            alt="Card 1"
+          />
+          <h1>Heart</h1>
+          <h3>The heart pumps blood through the circulatory system.</h3>
+        </div>
+        <div className="related-diseases">
+          <h5>Related Diseases</h5>
+          <ul>
+            {relatedDiseases.map((disease, index) => (
+              <li key={index}>{disease}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="right-section">
+        <div className="doctor-info">
+          <h3>Related Doctor</h3>
+          <p>{doctorDetails.nameDoctor}</p>
+          <p>Specialization: {doctorDetails.specialization}</p>
+          <p>Hospital: {doctorDetails.hospital}</p>
+        </div>
+        <form className="ask-question-form">
+          <h3>Ask a Question</h3>
+          <div>
+            <label>Your Question:</label>
+            <textarea rows="4"></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default OrganDetail;
