23cd5965fc5314c60f71c3b3af3bd7a45c9a5e59
sreedurgakakinada <sridurgakakinada@gmail.com>
2023-12-08 11:56:09 -0600

    Integrated Chatbot and added testcases

---
+++ FRONT-END/src/components/data.js
@@ -0,0 +1,310 @@
+// data.js
+
+const doctorClinicData = {
+    '10001': {
+      doctors: [
+        { name: 'Dr. Smith', specialty: 'Cardiologist' },
+        { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+      ],
+      clinics: [
+        { name: 'Healthy Clinic', address: '123 Main St' },
+        { name: 'Sunshine Clinic', address: '456 Elm St' },
+      ],
+    },
+    '10002': {
+      doctors: [
+        { name: 'Dr. Brown', specialty: 'Pediatrician' },
+        { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+      ],
+      clinics: [
+        { name: "Kid's Clinic", address: '789 Oak St' },
+        { name: 'Vision Care Center', address: '101 Pine St' },
+      ],
+    },
+    '10003': {
+        doctors: [
+          { name: 'Dr. Smith', specialty: 'Cardiologist' },
+          { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+        ],
+        clinics: [
+          { name: 'Healthy Clinic', address: '123 Main St' },
+          { name: 'Sunshine Clinic', address: '456 Elm St' },
+        ],
+    },
+    '10004': {
+        doctors: [
+          { name: 'Dr. Brown', specialty: 'Pediatrician' },
+          { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+        ],
+        clinics: [
+          { name: "Kid's Clinic", address: '789 Oak St' },
+          { name: 'Vision Care Center', address: '101 Pine St' },
+        ],
+      },
+      '10005': {
+        doctors: [
+          { name: 'Dr. Smith', specialty: 'Cardiologist' },
+          { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+        ],
+        clinics: [
+          { name: 'Healthy Clinic', address: '123 Main St' },
+          { name: 'Sunshine Clinic', address: '456 Elm St' },
+        ],
+      },
+      '10006': {
+        doctors: [
+          { name: 'Dr. Brown', specialty: 'Pediatrician' },
+          { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+        ],
+        clinics: [
+          { name: "Kid's Clinic", address: '789 Oak St' },
+          { name: 'Vision Care Center', address: '101 Pine St' },
+        ],
+      },
+      '10007': {
+          doctors: [
+            { name: 'Dr. Smith', specialty: 'Cardiologist' },
+            { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+          ],
+          clinics: [
+            { name: 'Healthy Clinic', address: '123 Main St' },
+            { name: 'Sunshine Clinic', address: '456 Elm St' },
+          ],
+        },
+        '10008': {
+          doctors: [
+            { name: 'Dr. Brown', specialty: 'Pediatrician' },
+            { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+          ],
+          clinics: [
+            { name: "Kid's Clinic", address: '789 Oak St' },
+            { name: 'Vision Care Center', address: '101 Pine St' },
+          ],
+        },
+        '10009': {
+            doctors: [
+              { name: 'Dr. Smith', specialty: 'Cardiologist' },
+              { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+            ],
+            clinics: [
+              { name: 'Healthy Clinic', address: '123 Main St' },
+              { name: 'Sunshine Clinic', address: '456 Elm St' },
+            ],
+          },
+          '10010': {
+            doctors: [
+              { name: 'Dr. Brown', specialty: 'Pediatrician' },
+              { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+            ],
+            clinics: [
+              { name: "Kid's Clinic", address: '789 Oak St' },
+              { name: 'Vision Care Center', address: '101 Pine St' },
+            ],
+          },
+          '10011': {
+              doctors: [
+                { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+              ],
+              clinics: [
+                { name: 'Healthy Clinic', address: '123 Main St' },
+                { name: 'Sunshine Clinic', address: '456 Elm St' },
+              ],
+            },
+            '10012': {
+              doctors: [
+                { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+              ],
+              clinics: [
+                { name: "Kid's Clinic", address: '789 Oak St' },
+                { name: 'Vision Care Center', address: '101 Pine St' },
+              ],
+            },
+            '10013': {
+              doctors: [
+                { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+              ],
+              clinics: [
+                { name: 'Healthy Clinic', address: '123 Main St' },
+                { name: 'Sunshine Clinic', address: '456 Elm St' },
+              ],
+            },
+            '10014': {
+              doctors: [
+                { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+              ],
+              clinics: [
+                { name: "Kid's Clinic", address: '789 Oak St' },
+                { name: 'Vision Care Center', address: '101 Pine St' },
+              ],
+            },
+            '10015': {
+                doctors: [
+                  { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                  { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                ],
+                clinics: [
+                  { name: 'Healthy Clinic', address: '123 Main St' },
+                  { name: 'Sunshine Clinic', address: '456 Elm St' },
+                ],
+              },
+              '10016': {
+                doctors: [
+                  { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                  { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                ],
+                clinics: [
+                  { name: "Kid's Clinic", address: '789 Oak St' },
+                  { name: 'Vision Care Center', address: '101 Pine St' },
+                ],
+              },
+              '10017': {
+                doctors: [
+                  { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                  { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                ],
+                clinics: [
+                  { name: 'Healthy Clinic', address: '123 Main St' },
+                  { name: 'Sunshine Clinic', address: '456 Elm St' },
+                ],
+              },
+              '10018': {
+                doctors: [
+                  { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                  { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                ],
+                clinics: [
+                  { name: "Kid's Clinic", address: '789 Oak St' },
+                  { name: 'Vision Care Center', address: '101 Pine St' },
+                ],
+              },
+              '10019': {
+                  doctors: [
+                    { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                    { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                  ],
+                  clinics: [
+                    { name: 'Healthy Clinic', address: '123 Main St' },
+                    { name: 'Sunshine Clinic', address: '456 Elm St' },
+                  ],
+                },
+                '10020': {
+                  doctors: [
+                    { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                    { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                  ],
+                  clinics: [
+                    { name: "Kid's Clinic", address: '789 Oak St' },
+                    { name: 'Vision Care Center', address: '101 Pine St' },
+                  ],
+                },
+                '10021': {
+                  doctors: [
+                    { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                    { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                  ],
+                  clinics: [
+                    { name: 'Healthy Clinic', address: '123 Main St' },
+                    { name: 'Sunshine Clinic', address: '456 Elm St' },
+                  ],
+                },
+                '10022': {
+                  doctors: [
+                    { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                    { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                  ],
+                  clinics: [
+                    { name: "Kid's Clinic", address: '789 Oak St' },
+                    { name: 'Vision Care Center', address: '101 Pine St' },
+                  ],
+                },
+                '10023': {
+                    doctors: [
+                      { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                      { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                    ],
+                    clinics: [
+                      { name: 'Healthy Clinic', address: '123 Main St' },
+                      { name: 'Sunshine Clinic', address: '456 Elm St' },
+                    ],
+                  },
+                  '10024': {
+                    doctors: [
+                      { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                      { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                    ],
+                    clinics: [
+                      { name: "Kid's Clinic", address: '789 Oak St' },
+                      { name: 'Vision Care Center', address: '101 Pine St' },
+                    ],
+                  },
+                  '10025': {
+                    doctors: [
+                      { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                      { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                    ],
+                    clinics: [
+                      { name: 'Healthy Clinic', address: '123 Main St' },
+                      { name: 'Sunshine Clinic', address: '456 Elm St' },
+                    ],
+                  },
+                  '10026': {
+                    doctors: [
+                      { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                      { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                    ],
+                    clinics: [
+                      { name: "Kid's Clinic", address: '789 Oak St' },
+                      { name: 'Vision Care Center', address: '101 Pine St' },
+                    ],
+                  },
+                  '10027': {
+                      doctors: [
+                        { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                        { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                      ],
+                      clinics: [
+                        { name: 'Healthy Clinic', address: '123 Main St' },
+                        { name: 'Sunshine Clinic', address: '456 Elm St' },
+                      ],
+                    },
+                    '10028': {
+                      doctors: [
+                        { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                        { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                      ],
+                      clinics: [
+                        { name: "Kid's Clinic", address: '789 Oak St' },
+                        { name: 'Vision Care Center', address: '101 Pine St' },
+                      ],
+                    },
+                    '10029': {
+                      doctors: [
+                        { name: 'Dr. Smith', specialty: 'Cardiologist' },
+                        { name: 'Dr. Johnson', specialty: 'Dermatologist' },
+                      ],
+                      clinics: [
+                        { name: 'Healthy Clinic', address: '123 Main St' },
+                        { name: 'Sunshine Clinic', address: '456 Elm St' },
+                      ],
+                    },
+                    '10030': {
+                      doctors: [
+                        { name: 'Dr. Brown', specialty: 'Pediatrician' },
+                        { name: 'Dr. Lee', specialty: 'Ophthalmologist' },
+                      ],
+                      clinics: [
+                        { name: "Kid's Clinic", address: '789 Oak St' },
+                        { name: 'Vision Care Center', address: '101 Pine St' },
+                      ],
+                    },
+
+    // Add more data for other pin codes
+  };
+
+export { doctorClinicData };
+  //export default doctorClinicData;
+  
